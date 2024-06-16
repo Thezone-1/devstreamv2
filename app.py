c815504591e05dec8cbd194efb14d5f1db6dfebf
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-06-17 01:51:58 +0530

    added heartbeat route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "hello, world"
+
+
+@app.route("/heartbeat")
+def heartbeat():
+    return jsonify({"status": "healthy"})
 
 
 @app.get("/welcome")
